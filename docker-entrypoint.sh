2d6c69e1e11b6a2cc7df59075ca81ee3a9f80522
George Svachulay <george.svachulay@opsforge.io>
2016-11-03 17:30:07 +0000

    Update docker-entrypoint.sh
    Fixed up nonsensical declarations....

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 # This is a custom entrypoint for the Rancherized Cabot. Don't change it.
-
-apt-get update
-apt-get install expect -y
-
-# Always run the prep script unconditionally as it does no harm but helps if something is broken
-
-bash bin/build-app
-python manage.py syncdb
-
-python manage.py createsuperuser --username=admin --email=admin@noreply.com --noinput
-
-if [[ $? -eq 0 ]] ; then
-  # User account did not exist, setting password from env var in Rancher
-  expect password.exp ${PASSWORD}
-fi
-
 if echo "$ROLE" | grep frontend ; then
   # Role is frontend, exec the relevant command
+  apt-get update
+  apt-get install expect -y
+  # Always run the prep script unconditionally as it does no harm but helps if something is broken
+  bash bin/build-app
+  python manage.py createsuperuser --username=admin --email=admin@noreply.com --noinput
+  if [[ $? -eq 0 ]] ; then
+    # User account did not exist, setting password from env var in Rancher
+    expect password.exp ${PASSWORD}
+  fi
+  python manage.py syncdb
   python manage.py runserver 0.0.0.0:80
 elif echo "$ROLE" | grep worker ; then
   # Role is frontend, exec the relevant command
